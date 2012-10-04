8e12bea60d33ab0480af1bad3729ec7d7ec2bd87
Danil Annenkov <support@ib-soft.ru>
2012-10-04 17:55:32 +0900

    Trying to add contraints

--- Transform.hs
+++ Transform.hs
@@ -1,11 +1,15 @@
 import Prelude hiding (id)
 import Data.List
+import Control.Arrow
 import Language.KURE
 import Language.KURE.Utilities
 
 -- Source AST
 data Model = Model { modelName  :: String
               , modelAttrs :: [Attr] }
+
+data Tag = PK | Required
+           deriving (Show, Eq)
 
 data Attr = Attr { attrName :: String
                  , attrType :: AttrType
@@ -14,9 +18,6 @@ data Attr = Attr { attrName :: String
 
 data AttrType = AttrInteger | AttrString | AttrFloat
                 deriving Show
-
-data Tag = PK | Required
-           deriving Show
 
 -- Target AST
 
@@ -27,10 +28,12 @@ instance Show Table where
     show (Table name cols) = "CREATE TABLE " ++ name ++ "(" ++ (concat $ intersperse ", " $ map show cols) ++ ")"
 
 data Column = Column { columnName :: String
-                     , columnType :: SQLType }
+                     , columnType :: SQLType 
+                     , constraints :: [SQLConstraint]
+                     , nullAllowed :: Bool }
 
 instance Show Column where
-    show (Column name colType) = name ++ " " ++ show colType
+    show (Column name colType constraints nullAllowed) = name ++ " " ++ show colType
 
 data SQLType = SQLInteger | SQLVarchar | SQLReal
 
@@ -39,6 +42,7 @@ instance Show SQLType where
     show SQLVarchar = "varchar"
     show SQLReal = "real"
 
+data SQLConstraint = PKConstraint { name :: String }                
 
 type TranslateE a b = Translate () KureMonad a b
 type TranslateAttr b = Translate () KureMonad Attr b
@@ -49,19 +53,43 @@ idR = translate $ \ _ -> return
 applyE :: TranslateE a b -> a -> Either String b
 applyE t = runKureMonad Right Left . apply t ()
 
+attrT = translate $ \_ (Attr name attrType tags)-> do
+          colType <- case attrType of
+                       AttrInteger -> return SQLInteger
+                       AttrString -> return SQLVarchar
+                       _ -> fail $ "Translate attribute " ++ name ++ " failed. No SQL equivalent for type: " ++ show attrType
+          constraints <- mapM (apply constraintT ()) tags
+          return $ Column name colType constraints False
+ 
+
+--pkT :: TranslateAttr Column
+pkT col = translate $ \_ (Attr name attrType tags) -> if (PK `elem` tags) then return col {nullAllowed = True}
+                                                       else  fail "" 
+
 translateAttr :: TranslateAttr Column
 translateAttr  = do
-  Attr name attrType _ <- idR
-  case attrType of
-    AttrInteger -> return $ Column name SQLInteger
-    AttrString -> return $ Column name SQLVarchar
-    _ -> fail $ "Translate attribute " ++ name ++ " failed. No SQL equivalent for type: " ++ show attrType
+  Attr name attrType tags <- idR
+  colType <- case attrType of
+               AttrInteger -> return SQLInteger
+               AttrString -> return SQLVarchar
+               _ -> fail $ "Translate attribute " ++ name ++ " failed. No SQL equivalent for type: " ++ show attrType
+  return $ Column name colType [] False
+
+constraintT = \_ tag -> case tag of
+                        PK -> return PKConstraint
+                        _ -> fail $ "No rule for tag: " ++ show tag
 
 translateModel :: TranslateModel Table
 translateModel = translate $ \_ (Model name attrs) -> do
                    cols <- apply trAttrs () attrs
                    return $ Table (name ++ "_table") cols
                    
+--translateClass = do
+--  Class name attrs <- idR
+--  columns <- trAttrs
+--  return $ Table (name ++ "_table") []
+
+--translateAttrType :: Attr -> Column
 translateAttr' attr =  case attr of
                             Attr name AttrInteger _ -> return $ Column name SQLInteger
                             _ -> fail "translateAttr fails"
@@ -73,5 +101,5 @@ sampleModel = Model {modelName = "Employee",
                                    Attr "name" AttrString [], 
                                    Attr "age" AttrInteger []]}
 trAttrs = do
-  attrs <- mapT translateAttr
-  return attrs
+  cols <- mapT translateAttr
+  return cols
